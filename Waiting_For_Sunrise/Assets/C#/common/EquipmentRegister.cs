9a7af125812db43f786e6008b649cedd763a74fe
q1anz1 <qianz0w0@outlook.com>
2025-11-29 16:16:56 +0800

    修改了AbstractRegister

--- Waiting_For_Sunrise/Assets/C#/common/EquipmentRegister.cs
+++ Waiting_For_Sunrise/Assets/C#/common/EquipmentRegister.cs
@@ -2,19 +2,19 @@ using Assets.C_.item;
 
 namespace Assets.C_.common
 {
-    public class EquipmentRegister : AbstractRegister
+    public class EquipmentRegister : AbstractRegister<FileResource, Equipment>
     {
         private static readonly string EQUIPMENT_JSON_FILE_RESOURCE_PATH = "D:\\develop\\UNITY\\projects\\Waiting_For_Sunrise\\Waiting_For_Sunrise\\Assets\\json\\equipment.json";
         private static readonly string EQUIPMENT_JSON_FILE_RESOURCE_TYPE = "content";
 
-        protected override object GetFileResource()
+        protected override FileResource GetFileResource()
         {
             return FileResourceFactory.CreateFileResource(EQUIPMENT_JSON_FILE_RESOURCE_PATH, EQUIPMENT_JSON_FILE_RESOURCE_TYPE);
         }
 
-        protected override IResourceManager<object, object> GetResourceManager()
+        protected override IResourceManager<FileResource, Equipment> GetResourceManager()
         {
-            return (IResourceManager<object, object>)EquipmentManager.GetInstance();
+            return EquipmentManager.GetInstance();
         }
     }
 }
