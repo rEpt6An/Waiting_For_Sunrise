0c47d500a72231b8b3b643f2abdd18cea33083c1
q1anz1 <qianz0w0@outlook.com>
2025-12-08 23:09:20 +0800

    为装备添加自动描述
    为装备添加自动描述

---
+++ Waiting_For_Sunrise/Assets/C#/common/util/DescriptionGenerater.cs
@@ -0,0 +1,31 @@
+
+using Assets.C_.item;
+using System.Net.PeerToPeer.Collaboration;
+
+namespace Assets.C_.common
+{
+    public class DescriptionGenerater
+    {
+        public static string GetEquipmentDescription(Equipment equipment)
+        {
+            return $"Max HP Increase: {equipment.MaxHP}\n" +
+                   $"Damage Multiplier: {equipment.DamageMultiple:P0}\n" +
+                   $"Max HP: {equipment.MaxHP}\n" +
+                   $"Defense: {equipment.DefensivePower}\n" +
+                   $"Dodge Rate: {equipment.Dodge:P2}\n" +
+                   $"Attack Speed: {equipment.AttackSpeed:F2}\n" +
+                   $"Hit Rate: {equipment.HitRate:P2}\n" +
+                   $"Vision Range: {equipment.Scope}\n" +
+                   $"Movement Speed: {equipment.Speed}\n" +
+                   $"Critical Chance: {equipment.CriticalChance:P2}\n" +
+                   $"Critical Damage Multiplier: {equipment.CriticalDamage:F1}x\n" +
+                   $"Life Steal: {equipment.Leech:P2}\n" +
+                   $"Self-Healing: {equipment.SelfHealing}\n" +
+                   $"Luck: {equipment.Lucky}\n" +
+                   $"Harvest Amount: {equipment.Harvest}\n" +
+                   $"Melee Attack: {equipment.MeleeAttack}\n" +
+                   $"Ranged Attack: {equipment.RangedAttack}";
+        }
+        
+    }
+}
