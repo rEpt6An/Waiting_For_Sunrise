d1b9f46d438a4a464042e87d7271da2760853d1e
窗外下雨了吗 <1136373263@qq.com>
2025-12-17 18:21:50 +0800

    Version1.0
    也是能够顺利进行了！好玩！

--- Waiting_For_Sunrise/Assets/C#/General/Quit.cs
+++ Waiting_For_Sunrise/Assets/C#/General/Quit.cs
@@ -9,14 +9,6 @@ public class Quit : MonoBehaviour
 {
     public void QuitGame()
     {
-//        Debug.Log("正在退出游戏...");
-
-#if UNITY_EDITOR
-        // 如果在Unity编辑器中，停止播放模式
-        UnityEditor.EditorApplication.isPlaying = false;
-#else
-        // 如果在构建的游戏中，退出应用程序
-        Application.Quit();
-#endif
+        UnityEngine.Application.Quit();
     }
 }
--- Waiting_For_Sunrise/Assets/C#/Player_inGame/Weapon/WeaponData.cs
+++ Waiting_For_Sunrise/Assets/C#/Player_inGame/Weapon/WeaponData.cs
@@ -1,4 +1,3 @@
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Weapon Data", menuName = "Data/Weapon Data")]
